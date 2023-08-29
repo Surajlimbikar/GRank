f06204ad8d0a5a4efa5c667807271f143f7ea3a8
Grank Website <141829550+GrankWebsite@users.noreply.github.com>
2023-08-29 11:36:46 +0530

    commit

--- src/layout/blanklayout/branding/BrandIdentity.js
+++ src/layout/blanklayout/branding/BrandIdentity.js
@@ -88,18 +88,6 @@ const BrandIdentity = () => {
         <div className="branding-process-div">
           <div className="container-fluid ">
             <div className="row">
-              <div className="col-12 col-md-6 col-lg-6 ">
-                <div className="branding-consulting-process-img">
-                  <motion.img
-                    initial={{ opacity: 0, scale: 0 }}
-                    whileInView={{ opacity: 1, scale: 1 }}
-                    transition={{ duration: 1 }}
-                    className="img-fluid branding-consulting-process-img"
-                    src="assest/photos/branding/branding-consulting-process-img.png"
-                    alt="G-Rank"
-                  />
-                </div>
-              </div>
               <div className="col-12 col-md-6 col-lg-6 branding-consulting-process-text">
                 <motion.h1
                   initial={{ opacity: 0, scale: 0 }}
@@ -132,6 +120,69 @@ const BrandIdentity = () => {
                   that aligns with the brand's messaging, and incorporate search
                   engine optimization (SEO) techniques to enhance visibility.
                 </motion.p>
+              </div>
+              <div className="col-12 col-md-6 col-lg-6 ">
+                {/* <div className="branding-consulting-process-img">
+                  <motion.img
+                    initial={{ opacity: 0, scale: 0 }}
+                    whileInView={{ opacity: 1, scale: 1 }}
+                    transition={{ duration: 1 }}
+                    className="img-fluid branding-consulting-process-img"
+                    src="assest/photos/branding/branding-consulting-process-img.png"
+                    alt="G-Rank"
+                  />
+                </div> */}
+                <div id="level">
+                  <div id="content">
+                    <div id="gears">
+                      <div id="gears-static"></div>
+                      <div id="gear-system-1">
+                        <div class="shadow" id="shadow15"></div>
+                        <div id="gear15"></div>
+                        <div class="shadow" id="shadow14"></div>
+                        <div id="gear14"></div>
+                        <div class="shadow" id="shadow13"></div>
+                        <div id="gear13"></div>
+                      </div>
+                      <div id="gear-system-2">
+                        <div class="shadow" id="shadow10"></div>
+                        <div id="gear10"></div>
+                        <div class="shadow" id="shadow3"></div>
+                        <div id="gear3"></div>
+                      </div>
+                      <div id="gear-system-3">
+                        <div class="shadow" id="shadow9"></div>
+                        <div id="gear9"></div>
+                        <div class="shadow" id="shadow7"></div>
+                        <div id="gear7"></div>
+                      </div>
+                      <div id="gear-system-4">
+                        <div class="shadow" id="shadow6"></div>
+                        <div id="gear6"></div>
+                        <div id="gear4"></div>
+                      </div>
+                      <div id="gear-system-5">
+                        <div class="shadow" id="shadow12"></div>
+                        <div id="gear12"></div>
+                        <div class="shadow" id="shadow11"></div>
+                        <div id="gear11"></div>
+                        <div class="shadow" id="shadow8"></div>
+                        <div id="gear8"></div>
+                      </div>
+                      <div class="shadow" id="shadow1"></div>
+                      <div id="gear1"></div>
+                      <div id="gear-system-6">
+                        <div class="shadow" id="shadow5"></div>
+                        <div id="gear5"></div>
+                        <div id="gear2"></div>
+                      </div>
+                      <div class="shadow" id="shadowweight"></div>
+                      <div id="chain-circle"></div>
+                      <div id="chain"></div>
+                      <div id="weight"></div>
+                    </div>
+                  </div>
+                </div>
               </div>
             </div>
           </div>
